fb6201322dfc1bc759ad3c41a08d0d4b2e89eab4
Дмитрий Нагирняк <dmitrynagirnyak@MacBook-Air-Dmitrij.local>
2025-07-15 18:46:54 +0300

    Добавлен ряд функций и README

--- src/file_logger.cpp
+++ src/file_logger.cpp
@@ -1,54 +1,58 @@
 #include "logger.h"
-#include <mutex>
-#include <fstream>
 #include <chrono>
+#include <fstream>
 #include <iomanip>
+#include <mutex>
 
 class FileLogger : public ILogger {
 private:
-    std::string m_filename;
-    std::mutex m_mutex;
-    LogLevel m_logLevel;
+  std::string m_filename;
+  std::mutex m_mutex;
+  LogLevel m_logLevel;
 
-    std::string levelToString(LogLevel level){
-        switch(level)
-        {
-        case LogLevel::DEBUG:
-            return "DEBUG";
-        case LogLevel::INFO:
-            return "INFO";
-        case LogLevel::WARNING:
-            return "WARNING";
-        case LogLevel::ERROR:
-            return "ERROR";    
-        default:
-            return "UNKNOWN";
-        }
+  std::string levelToString(LogLevel level) {
+    switch (level) {
+    case LogLevel::DEBUG:
+      return "DEBUG";
+    case LogLevel::INFO:
+      return "INFO";
+    case LogLevel::WARNING:
+      return "WARNING";
+    case LogLevel::ERROR:
+      return "ERROR";
+    default:
+      return "UNKNOWN";
     }
+  }
 
 public:
-    FileLogger(const std::string& filename, LogLevel level) : m_filename(filename), m_logLevel(level) {}
+  FileLogger(const std::string &filename, LogLevel level)
+      : m_filename(filename), m_logLevel(level) {}
 
-    void log(const std::string& message, LogLevel level) override {
-        if (level < m_logLevel) return;
+  void log(const std::string &message, LogLevel level) override {
+    if (level < m_logLevel)
+      return;
 
-        std::lock_guard<std::mutex> lock(m_mutex);
-        std::ofstream file(m_filename, std::ios::app);
-        if (!file.is_open()) {
-            throw std::runtime_error("Cannot open the file" + m_filename);
-        }
-
-        auto now = std::chrono::system_clock::now();
-        auto time = std::chrono::system_clock::to_time_t(now);
-
-        file << "[" << std::put_time(std::localtime(&time), "%Y-%m-%d %H:%M:%S") << "] " << "[" << levelToString(level) << "] " << message << std::endl;
+    std::lock_guard<std::mutex> lock(m_mutex);
+    std::ofstream file(m_filename, std::ios::app);
+    if (!file.is_open()) {
+      throw std::runtime_error("Cannot open the file" + m_filename);
     }
 
-    void setLogLevel(LogLevel level) override {
-        m_logLevel = level;
-    }
+    auto now = std::chrono::system_clock::now();
+    auto time = std::chrono::system_clock::to_time_t(now);
 
-    LogLevel getLogLevel() const override {
-        return m_logLevel;
-    }
+    file << "[" << std::put_time(std::localtime(&time), "%Y-%m-%d %H:%M:%S")
+         << "] " << "[" << levelToString(level) << "] " << message << std::endl;
+  }
+
+  void setLogLevel(LogLevel level) override { m_logLevel = level; }
+
+  LogLevel getLogLevel() const override { return m_logLevel; }
+
+  // Функция для создания логгера
+  std::unique_ptr<ILogger> createFileLogger(const std::string &filename,
+                                            LogLevel level) {
+    return std::make_unique<FileLogger>(filename, level);
+  }
 };
